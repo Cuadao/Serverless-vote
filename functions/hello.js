e1ec51a329c7ec4ede3e8d3b79581d602681e227
Camilo Cely <59616536+Cuadao@users.noreply.github.com>
2020-11-07 12:33:47 -0500

    first commit

---
+++ functions/hello.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+exports.handler = async (event) => {
+  const subject = event.queryStringParameters.name || "World";
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`,
+  };
+};
